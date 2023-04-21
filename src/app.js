535fdcbda60caf67c7230fb8c3539378dd6e3368
nkbtemmy <nkubito@augmented-future.com>
2023-04-21 22:15:02 +0200

    add cors in app

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@
 import express from "express";
 import session from "express-session";
 import dotenv from "dotenv";
+import cors from "cors";
 import DB from "./database";
 import router from "./restful/routes/index";
 import fileUploader from "express-fileupload";
@@ -14,6 +15,7 @@ const PORT = process.env.PORT || 4000;
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 app.use(express.json());
 app.use(
   session({
