a6bc20eebbf1b6ff5bc0eddcfa477f954f11a443
nkbtemmy <nkubito@augmented-future.com>
2023-05-16 17:39:50 +0200

    make it better

--- src/services/reviewServices.js
+++ src/services/reviewServices.js
@@ -9,43 +9,42 @@ export default class ReviewService {
    */
 
   static async create(param) {
-      const review = await Review.create(param);
-      return review;
+    const review = await Review.create(param);
+    return review;
   }
 
   static async findONE(param) {
-      const Reviews = await Review.findOne({
-        where: param,
-      });
-      return Reviews;
+    const Reviews = await Review.findOne({
+      where: param,
+    });
+    return Reviews;
   }
 
   static async findAll() {
-      const Reviews = await Review.findAll({
-        include: [
-          {
-            model: User,
-            as: "reviewer",
-            attributes: ["displayName", "email", "role"],
-          },
-          {
-            model: User,
-            as: "reviewee",
-            attributes: ["displayName", "email", "role"],
-          },
-        ],
-      });
-      return Reviews;
+    const Reviews = await Review.findAll({
+      include: [
+        {
+          model: User,
+          as: "reviewer",
+          attributes: ["displayName", "email", "role"],
+        },
+        {
+          model: User,
+          as: "reviewee",
+          attributes: ["displayName", "email", "role"],
+        },
+      ],
+    });
+    return Reviews;
   }
 
   static async findById(id) {
-      const Review = await Review.findByPk(id);
-      return Review;
+    const Review = await Review.findByPk(id);
+    return Review;
   }
 
   static async delete(id) {
-      const review = await Review.destroy(id);
-      return review;
+    const review = await Review.destroy(id);
+    return review;
   }
-  
 }
